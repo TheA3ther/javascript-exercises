3ebdca6d3285c0a84a94eb3e1109eeba97b0fb36
TheA3ther <carlosroycanoperez@gmail.com>
2024-09-25 13:55:30 +0800

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const argsSet = new Set(args);
+    return arr.filter(item => !argsSet.has(item));
 };
 
 // Do not edit below this line
