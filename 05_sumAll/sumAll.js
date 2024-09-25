311db119bf63fd67533f769eb46413f9ba45dda1
TheA3ther <carlosroycanoperez@gmail.com>
2024-09-25 14:07:22 +0800

    Complete Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) return 'ERROR';
+    let [start, end] = x < y ? [x, y] : [y, x];
+    let sum = 0;
+    for (i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
