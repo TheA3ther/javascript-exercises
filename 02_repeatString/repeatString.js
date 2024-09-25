83ad8953e402f8d6a4c1ae9f932b2f02c2b4c103
TheA3ther <carlosroycanoperez@gmail.com>
2024-09-25 13:25:17 +0800

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (count < 0) return "ERROR";
+    arr = new Array(count).fill(string);
+    return arr.join('');
 };
 
 // Do not edit below this line
