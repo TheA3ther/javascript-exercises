ae0aa433a647e1dc4a703dc8319ebddd24a7c179
TheA3ther <carlosroycanoperez@gmail.com>
2024-10-02 17:09:36 +0800

    Complete Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let normalizedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let j = normalizedStr.length - 1;
+    for (i = 0; i < normalizedStr.length; i++){
+        if (i >= j) break;
+        if (normalizedStr[i] != normalizedStr[j--]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
