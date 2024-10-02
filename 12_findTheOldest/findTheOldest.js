187a7ce30c491e0590253f1e11e4c0158ee15638
TheA3ther <carlosroycanoperez@gmail.com>
2024-10-02 17:29:18 +0800

    Complete Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, curr) =>
+        (("yearOfDeath" in oldest ? oldest.yearOfDeath : new Date().getFullYear()) - oldest.yearOfBirth) > 
+        (("yearOfDeath" in curr ? curr.yearOfDeath : new Date().getFullYear()) - curr.yearOfBirth) ? oldest : curr
+    );
+}
 // Do not edit below this line
 module.exports = findTheOldest;
