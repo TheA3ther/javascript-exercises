b494a3a1904968eea7367b1b60699f76592973d7
TheA3ther <133721155+TheA3ther@users.noreply.github.com>
2026-08-06 16:27:12 +0800

    Solve foundations/08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let blacklist = new Set(args);
+    return arr.filter((num) => !(blacklist.has(num)));
 };
 
 // Do not edit below this line
