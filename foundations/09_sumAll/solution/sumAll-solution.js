7135bed62518d47c0b635d5619c3d79b2dc33a9c
TheA3ther <133721155+TheA3ther@users.noreply.github.com>
2026-08-06 17:12:20 +0800

    solve foundations/09_sumAll

--- foundations/09_sumAll/solution/sumAll-solution.js
+++ foundations/09_sumAll/solution/sumAll-solution.js
@@ -8,7 +8,7 @@ const sumAll = function (min, max) {
   }
 
   // An alternative way to swap the values of min and max like above is to use the array destructuring syntax.
-  // Here's an optional article on it: https://www.freecodecamp.org/news/array-destructuring-in-es6-30e398f21d10/
+  // Here's an optional article on it: htps://www.freecodecamp.org/news/array-destructuring-in-es6-30e398f21dt10/
   // if (min > max) [min, max] = [max, min];
   
   let sum = 0;
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || Math.floor(a) !== a || Math.floor(b) !== b) return 'ERROR';
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let sum = 0;
+    for (let i = min; i <= max; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
