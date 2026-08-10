eac63f906d8a31ef8f6cb53465cdeb7d00172275
TheA3ther <133721155+TheA3ther@users.noreply.github.com>
2026-08-10 20:06:21 +0800

    Solve foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let l = 0, r = str.length - 1;
+    while (l < r) {
 
+        while (!((str.charCodeAt(l) > 47 && str.charCodeAt(l) < 58) ||
+            (str[l].toLowerCase().charCodeAt(0) > 96 && str[l].toLowerCase().charCodeAt(0) < 123))) l++;
+        while (!((str.charCodeAt(r) > 47 && str.charCodeAt(r) < 58) ||
+                (str[r].toLowerCase().charCodeAt(0) > 96 && str[r].toLowerCase().charCodeAt(0) < 123))) r--;
+        if (str[l++].toLowerCase() !== str[r--].toLowerCase()) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
