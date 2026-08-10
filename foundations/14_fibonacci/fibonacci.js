7055bb61c496cc635d454ee1779d89e7da5f3a76
TheA3ther <133721155+TheA3ther@users.noreply.github.com>
2026-08-10 20:21:42 +0800

    Solve foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(f) {
+    if (f < 0) return 'OOPS';
+    switch (+f){
+        case 0: 
+            return 0;
+            break;
+        case 1:
+        case 2:
+            return 1;
+            break;
+        default:
+            return fibonacci(Math.max(f - 1, 0)) + fibonacci(Math.max(f - 2, 0)); 
+    }
 };
 
 // Do not edit below this line
