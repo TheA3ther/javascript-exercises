4a799029cc0f2c5fa22a3cf1ef39bd51c678aa29
TheA3ther <133721155+TheA3ther@users.noreply.github.com>
2026-08-10 19:44:50 +0800

    Solve foundations/12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((a, c) => a + c, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a, c) => a * c, 1);
 };
 
-const power = function() {
-	
+const power = function(b, e) {
+	return b ** e;
 };
 
-const factorial = function() {
-	
+const factorial = function(f) {
+	let res = 1;
+  while (f > 1) res = res * f--;
+  return res;
 };
 
 // Do not edit below this line
