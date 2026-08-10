522f2e72d4c4b729219768127359c210911785b4
TheA3ther <133721155+TheA3ther@users.noreply.github.com>
2026-08-10 20:46:07 +0800

    Solve foundations/16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((a, c) => {
+        return ((c.yearOfDeath ?? new Date().getFullYear())- c.yearOfBirth) > ((a.yearOfDeath ?? new Date().getFullYear())- a.yearOfBirth) ? c : a
+    });
 };
 
 // Do not edit below this line
